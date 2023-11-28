86ecd1d6a70a9524e189bb54a1d9f8a0f34bc979
sahal2000 <me.sahal2000@gmail.com>
2023-11-28 15:01:10 +0600

    ads

---
+++ showOutput.js
@@ -0,0 +1,12 @@
+// using alert
+// alert("Anisul Islam");
+
+// write function
+// document.write("I love BD");
+document.write("<h1>Hm</h1>");
+document.write("Name : Md Sahal <br/>");
+document.write("Phone : 01203123122");
+
+// console.log
+console.log("I love bd");
+
